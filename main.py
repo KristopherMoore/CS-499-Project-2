6ecc743e4174e48bb61a332349c94a147979a9ce
Jake Farrar <jakefarrar@Jakes-MacBook-Air.local>
2019-05-08 23:28:48 -0700

    Fixed the alignment

--- main.py
+++ main.py
@@ -14,21 +14,21 @@ import itertools
 
 #main module responsible for handling dataset and calling functions
 def main():
-	
+
     #initializations
     executionFlag = True
     targetSequenceFileName = ""
     readSequenceFileName = ""
-   
+
     #greeting message
     print("\nCS:499-Bioinformatics, Local Alignment Using Seeds Tool:\n_____________________________________________________\n\n")
-   
+
     #request location of TARGET sequence from user (The sequence we want to align to)
     targetSequenceFileName = requestSequence("TARGET")
-   
+
     #request location of READ sequence from user (The sequence we want to use for alignment)
     readSequenceFileName = requestSequence("READ")
-   
+
     #program loop, only allow exit when the user has specifically requested to do so
     while executionFlag:
         print("\nSelect a function to Execute\n\n"
@@ -36,39 +36,39 @@ def main():
         +"\t2: Change (Target)Sequence\n"
         +"\t3: Change (Read)Sequence\n"
         +"\t0: Quit\n")
-        
+
         userInput = input("\tENTER NUMBER:")
-      
+
         #check userChoices
         if(userInput == "0"):
             print("\nEXITING...")
             executionFlag = False
-      
+
 		#main tool function, initiate all actions
         elif(userInput == "1"):
             print("\nReading Target Sequence, Creating Target dictionary...")
             targetDict = readSequences(targetSequenceFileName)
-         
+
             print("\nGenerating Seeds from Read Sequence, Creating Seeds dictionary...")
             seedsDict = generateSeeds(readSequenceFileName)
-		 
+
             print("\nMatching Seeds on our Target, Creating Matched dictionary...")
             matchedDict = matchSeedOnReference(targetDict, seedsDict)
-		 
+
             print("\nExtending matches, Creating FinalSeeds dictionary...")
             finalDict = extendMatchedSeeds(targetDict, matchedDict)
-		 
+
             print("\nPerforming Final Local Alignment...")
             localAlignment(targetDict, finalDict)
-      
-        #allow users to modify the Target Sequence Location	  
+
+        #allow users to modify the Target Sequence Location
         elif(userInput == "2"):
             targetSequenceFileName = requestSequence("TARGET")
-      
+
         #allow users to modify the Read Sequence Location
         elif(userInput == "3"):
             readSequenceFileName = requestSequence("READ")
-      
+
         #invalid input
         else:
             print("Invalid Selection, Please select from the list")
@@ -88,13 +88,13 @@ def readSequences( file_name ):
             if '>' in line:
                 key = line.strip('\n')
                 tmp[key] = ""
-            else:   
+            else:
                 tmp[key] = tmp[key] + line.strip('\n')
 
             line = fasta.readline()
-            
+
     return tmp
-	
+
 
 
 
@@ -103,18 +103,18 @@ def readSequences( file_name ):
 #and utilzing the K-Mer Exact Match Seeding process to develop K-mer seeds to be
 #matched upon our reference sequence, return dictionary of seeds to calling method
 def generateSeeds( file_name ):
-	
+
    sequence = readSequences( file_name )
    dict = {}
-   
+
    #loop through each element, then at each element parse possibility of string of current index to index + k.
    for element in sequence:
       subString = ""
       byteSubString = b""
-      
+
       if '>' in element:
         continue
-      
+
       i = int(element)
       while i < element + k:
          if(element + k > len(sequence)):
@@ -122,16 +122,16 @@ def generateSeeds( file_name ):
          if(sequence[i] == "\n"):
             i = element + k  #ensure we escape our while loop, no relevent mers to be found here
             break
-         
+
          subString += sequence[i]
          i = i+1
-      
+
       #check if the key already exists if so then add to its count, else make the count 1
       if subString in dict:
          dict[subString] = dict.get(subString, 0) + 1
       elif(len(subString) == k):
          dict[subString] = 1
-         
+
    return dict
 
 
@@ -148,7 +148,7 @@ def matchSeedOnReference( reference, seeds ):
             if seed in refString and seed not in matched.values():
                 matched[index] = seed
                 index += 1
-		
+
     return matched
 
 
@@ -201,7 +201,6 @@ def sw(a, b, match_score=3, gap_cost=2):
 
     H = matrix(a, b, match_score, gap_cost)
     b_, pos = traceback(H, b)
-
     return pos, pos + len(b_)
 
 
@@ -211,12 +210,15 @@ def sw(a, b, match_score=3, gap_cost=2):
 #localAlignment is responsible for perfoming the final local sequence alignment of the
 #matched and extended seeds to the reference sequence(s), utilizing the Smith-Waterman Algorithm
 def localAlignment( reference, finalSeeds ):
-	begining , ending = sw(reference,finalSeeds);
-	print(reference[begining:ending]);
-	
-	
-	
-#helper function to request a sequence from the user   
+	for key in reference:
+		refString = str(reference[key])
+		finalString = str(finalSeeds)
+		begining , ending = sw(refString,finalString);
+		print(reference[key][begining:ending]);
+
+
+
+#helper function to request a sequence from the user
 def requestSequence(fileType):
    UI = input("\nEnter File name for the (" + str(fileType) + ") Sequence (.FASTA format): ")
    return UI
